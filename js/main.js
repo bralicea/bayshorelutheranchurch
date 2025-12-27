c6564eeebc1d2e9ec1009f32769ebb420598ef6b
bralicea <aliceabryan24@gmail.com>
2025-12-27 00:26:40 -0500

    speed changes

--- js/main.js
+++ js/main.js
@@ -33,54 +33,6 @@
     };
     loader();
 
-    var carousel = function () {
-        $('.home-slider').owlCarousel({
-            loop: true,
-            autoplay: true,
-            margin: 0,
-            animateOut: 'fadeOutLeft',
-            animateIn: 'fadeInDown',
-            nav: true,
-            dots: true,
-            autoplayTimeout: 6000,
-            autoplayHoverPause: false,
-            items: 1,
-            navText: ["<span class='ion-ios-arrow-back'></span>", "<span class='ion-ios-arrow-forward'></span>"],
-            responsive: {
-                0: {
-                    items: 1
-                },
-                600: {
-                    items: 1
-                },
-                1000: {
-                    items: 1
-                }
-            }
-        });
-        $('.carousel-testimony').owlCarousel({
-            center: true,
-            loop: true,
-            items: 1,
-            margin: 30,
-            stagePadding: 0,
-            nav: false,
-            navText: ['<span class="ion-ios-arrow-back">', '<span class="ion-ios-arrow-forward">'],
-            responsive: {
-                0: {
-                    items: 1
-                },
-                600: {
-                    items: 2
-                },
-                1000: {
-                    items: 3
-                }
-            }
-        });
-
-    };
-    carousel();
 
     $('nav .dropdown').hover(function () {
         var $this = $(this);
@@ -200,12 +152,6 @@
     };
     contentWayPoint();
 
-    $('.appointment_date').datepicker({
-        'format': 'm/d/yyyy',
-        'autoclose': true
-    });
-
-    $('.appointment_time').timepicker();
 
 })(jQuery);
 
