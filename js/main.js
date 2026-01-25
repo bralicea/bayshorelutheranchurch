53d93b495d265ed21f8d29db7e4705059cbc3859
bralicea <aliceabryan24@gmail.com>
2026-01-25 13:41:36 -0500

    fix mobile scrolling 2

--- js/main.js
+++ js/main.js
@@ -2,17 +2,6 @@
 
     "use strict";
 
-
-    $(window).stellar({
-        responsive: true,
-        parallaxBackgrounds: true,
-        parallaxElements: true,
-        horizontalScrolling: false,
-        hideDistantElements: false,
-        scrollProperty: 'scroll'
-    });
-
-
     var fullHeight = function () {
 
         $('.js-fullheight').css('height', $(window).height());
